30d7ed8c1e2925b242bc7380418b2016cf318ad1
Everton R. Auler <evertonrobertoauler@gmail.com>
2014-06-07 19:33:28 -0300

    Some ajusts

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,24 +5,27 @@ angular.module('ideaApp')
     $scope.title = 'Idea App';
   })
   .controller('IdeaCtrl', function($scope, $http) {
-    $http.get('/api/view.json').success(function(view) {
 
-      $scope.navbar = view.navbar;
-      $scope.form = view.form;
-
-      $scope.submit = function() {
-        console.log(JSON.stringify($scope.form));
-      };
-
-      $scope.reset = function() {
-        ($scope.form.fields || []).forEach(function(field) {
-          field.value = '';
-        });
-      };
-
-      $scope.validate = function() {
-        $scope.form.showErrors = true;
-        return $scope.form.form.$valid;
-      };
+    $http.get('/api/navbar.json').success(function(navbar) {
+      $scope.navbar = navbar;
     });
+
+    $http.get('/api/form.json').success(function(form) {
+      $scope.form = form;
+    });
+
+    $scope.submit = function() {
+      console.log(JSON.stringify($scope.form));
+    };
+
+    $scope.reset = function() {
+      ($scope.form.fields || []).forEach(function(field) {
+        field.value = '';
+      });
+    };
+
+    $scope.validate = function() {
+      $scope.form.showErrors = true;
+      return $scope.form.form.$valid;
+    };
   });
